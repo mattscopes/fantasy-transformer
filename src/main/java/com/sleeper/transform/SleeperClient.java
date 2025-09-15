471c317359beb543cc1650b7adf2776a32faff93
Matthew Scoppa <matthewscoppa@gmail.com>
2025-09-15 15:48:41 -0400

    small revision

--- src/main/java/com/sleeper/transform/SleeperClient.java
+++ src/main/java/com/sleeper/transform/SleeperClient.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.sleeper.transform.models.League;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,19 +15,17 @@ import java.net.http.HttpResponse;
 public class SleeperClient {
     
     private final HttpClient client;
-    private final String leagueId;
     
     private static final String BASE_URL = "https://api.sleeper.app/v1/";
     
     public static final ObjectMapper mapper = new ObjectMapper()
         .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
     
-    public SleeperClient(HttpClient client, String leagueId) {
+    public SleeperClient(@NonNull HttpClient client) {
          this.client = client;
-         this.leagueId = leagueId;
     }
     
-    public League getLeagueData() throws IOException, InterruptedException {
+    public League getLeagueData(@NonNull String leagueId) throws IOException, InterruptedException {
         String url = BASE_URL + "league/" + leagueId;
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(url))
